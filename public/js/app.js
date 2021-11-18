f1762ec8144eb1594ddca88ffc9e0f64b1a4b5c6
Sam BZEZ <sam@bzez.dev>
2021-11-19 00:07:20 +0100

    Init..

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,53 @@
 document.querySelector('#menu').addEventListener('show.bs.offcanvas', function () {
-   setTimeout(function (){
-       genLink('http://localhost/sfrbox/public/@result');
-       document.querySelector('[data-bs-dismiss="offcanvas"]').click()
-   },1500)
+
+    // Get the video element
+
+// Check if device has camera
+    /*navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+    navigator.getMedia({video: !0, audio: !1}, function (stream) {
+        if ('srcObject' in video) {
+            video.srcObject = stream;
+        } else {
+            video.src = vu.createObjectURL(stream);
+        }
+        video.play();
+    }, function (error) {
+        if (window.console)
+            console.error(error);
+    });
+
+    video.addEventListener('canplay', function (ev) {
+        if (!streaming) {
+            video.setAttribute('style', 'margin-left:-33vh;');
+            height = video.videoHeight / (video.videoWidth / width);
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            streaming = !0;
+        }
+    }, !1);*/
 })
 document.querySelector('#menu').addEventListener('hide.bs.offcanvas', function () {
+    const video = document.querySelector('#video')
+    video.srcObject = null;
     disableSpinners(document.querySelector('[data-bs-toggle="offcanvas"]'))
 })
-document.body.querySelector('#app').addEventListener('hide.bs.modal',function () {
+document.body.querySelector('#app').addEventListener('hide.bs.modal', function () {
     console.log(window.location.href.split('@')[0])
     unsetUrl()
 })
-window.onpopstate = function(event) {
-        genLink(document.location)
+window.onpopstate = function (event) {
+    genLink(document.location)
 };
 window.onresize = () => {
     console.log('resize')
     setPadding()
 }
-/*setButtonsEvent();
-setLinksEvent();*/
+setButtonsEvent();
+setLinksEvent();
 document.body.style.display = 'block';
 
-function toggleResult(){
+function toggleResult() {
     fake_photo.remove();
     fake_res.classList.remove('d-none')
 }
