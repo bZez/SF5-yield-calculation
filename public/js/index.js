f157ed08f137f7759fbd1da8f30e9c0a728d027e
Sam BZEZ <sam@bzez.dev>
2021-11-19 16:09:22 +0100

    Init..

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/js/sw.js')
+        .register('sw.js')
         .then(() => { console.log('Service Worker Registered'); });
 }
 
