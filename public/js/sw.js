3cfab0e3ba8433a3773b07502b4ddf39ed4084e4
Sam BZEZ <sam@bzez.dev>
2021-11-19 12:49:09 +0100

    Init..

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,21 +1,14 @@
-var cacheName = 'hello-pwa';
-var filesToCache = ['/'];
-
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
-    self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      '/'
+    ])),
+  );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
 });
